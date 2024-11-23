3916fe3ff646a02421ca5de82c65a89e450b7b6d
Carlos Cano <caknom@gmail.com>
2024-11-23 00:08:55 -0500

    scroll animation
    In this commint, I made the scroll animation for the hero section and made some minor changes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,48 @@
 (() => {
+
+  const canvas = document.querySelector("#explode-view");
+  const context = canvas.getContext("2d");
+
+  canvas.width = 1920;
+  canvas.height = 1080;
+
+  const frameCount = 721;
+  const images = [];
+
+  for(let i = 0; i < frameCount; i++) {
+      const img = new Image();
+      img.src = `images/sequence/snaky_video${(i+1).toString().padStart(3, '0')}.jpg`;
+      images.push(img);
+  }
+
+  const buds = {
+      frame: 0
+  };
+
+  gsap.to(buds, {
+      frame: 705,
+      snap: "frame",
+      scrollTrigger: {
+          trigger: "#explode-view",
+          pin: true,
+          scrub: 1,
+          // markers: true,
+          start: "top top"
+      },
+      onUpdate: render
+  });
+
+  images[0].addEventListener("load", render);
+  
+  function render() {
+      console.log(images[buds.frame]);
+      context.clearRect(0, 0, canvas.width, canvas.height);
+      context.drawImage(images[buds.frame], 0, 0);
+  };
+
+})();
+
+(() => {
   const model = document.querySelector("#model");
   const hotspots = document.querySelectorAll(".Hotspot");
 
